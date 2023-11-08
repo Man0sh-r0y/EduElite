53ac756d428aad376175091612a09c62510718ec
Manash Roy <manashroy454@gmail.com>
2023-11-08 22:55:04 +0530

    first commit

---
+++ server/README.md
@@ -0,0 +1,1 @@
+# Backend of Edtech Project
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-3xl font-bold text-center text-white">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
