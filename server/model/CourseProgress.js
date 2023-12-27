eb4e2aa7c3c18952b6ea8a2db3a47f937807e32d
Manash Roy <manashroy454@gmail.com>
2023-12-27 22:17:42 +0530

    updated

--- server/model/CourseProgress.js
+++ server/model/CourseProgress.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');// importing mongoose library
 
+// for each course, there will be a course progress bar in the student's account
+// it will have the corresponding courseID and the completed videos of that course
+// completed videos means the videos that the student has watched  
+
+// creating a schema for course progress
 const courseProgressSchema = new mongoose.Schema({
     courseID: {
         type: mongoose.Schema.Types.ObjectId, // defining the data type
