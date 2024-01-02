ad7d07a53b1e6acc0f063c152af83b4b91a4f160
Manash Roy <manashroy454@gmail.com>
2024-01-02 14:42:35 +0530

    updated database.js

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose'); // importing mongoose library
 require('dotenv').config(); // configuring dotenv to use .env file
 
 // connecting to the database
-function connect(){
+function dbConnect(){
     mongoose.connect(process.env.MONGODB_URL,{
         useNewUrlParser: true, // specifies whether to use the new MongoDB connection string parser
         useUnifiedTopology: true // indicates that Mongoose should use the new Unified Topology engine
@@ -16,5 +16,5 @@ function connect(){
     });
 }
 
-module.exports = { connect }; // exporting the connect function
+module.exports = dbConnect; // exporting the connect function
   
