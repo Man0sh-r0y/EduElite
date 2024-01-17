3abe8cd047d73fa6eed5907bac2ed0a9d75a91cf
Manash Roy <manashroy454@gmail.com>
2024-01-17 20:38:32 +0530

    updated the file

--- server/controllers/CourseContentSubSection.js
+++ server/controllers/CourseContentSubSection.js
@@ -7,13 +7,13 @@ exports.createSubSection = async (req, res) => {
 
     try {
         // Fecth data from Req body
-        const { sectionId, title, timeDuration, description } = req.body;
+        const { sectionId, title, description } = req.body;
 
         // Extract file/video from req.files
         const video = req.files.videoFile;
 
         // Validation of data
-        if (!sectionId || !title || !timeDuration || !description || !video) {
+        if (!sectionId || !title || !description || !video) {
             return res.status(400).json({
                 success: false,
                 message: "Please check all the required fields. Some fields are missing."
@@ -26,9 +26,9 @@ exports.createSubSection = async (req, res) => {
         //create a sub-section
         const subSectionDetails = await CourseContentSubSection.create({
             title: title,
-            timeDuration: timeDuration,
             description: description,
-            videoUrl: videoUploadDetails.secure_url,
+            timeDuration: `${videoUploadDetails.duration}`,
+            videoUrl: videoUploadDetails.secure_url
         });
 
         // Update the Course Content Section with this Course Content Sub Section ObjectId
@@ -54,6 +54,93 @@ exports.createSubSection = async (req, res) => {
     }
 };
 
-//HW: updateSubSection
+// Update the Course Content SubSection
+exports.updateSubSection = async (req, res) => {
 
-//HW:deleteSubSection
+    try {
+        // Fecth data from Req body
+        const { subSectionId, title, description } = req.body;
+
+        // Fetch the sub-section
+        const courseContentSubSection = await CourseContentSubSection.findById(subSectionId);
+
+        // Validation of data
+        if(!courseContentSubSection) {
+            return res.status(404).json({
+                success: false,
+                message: "Sub Section not found"
+            })
+        }
+
+        // Validating each data field seperately as user can update any field or all fields
+        if(title !== undefined){
+            courseContentSubSection.title = title; // if title is present in req.body then update the title
+        }
+
+        if(description !== undefined){
+            courseContentSubSection.description = description; // if description is present in req.body then update the description
+        }
+
+        // Extract file/video from req.files
+        if (req.files && req.files.videoFile !== undefined) { // if video is present in req.files then update the video
+            const video = req.files.videoFile;
+            const uploadVideoDetails = await uploadMediaToCloudinary(video, process.env.FOLDER_NAME);
+            courseContentSubSection.videoUrl = uploadVideoDetails.secure_url; // update the video url
+            courseContentSubSection.timeDuration = `${uploadVideoDetails.duration}`; // As Schema is expecting timeDuration in string format
+        }
+
+        // Save the updated sub-section
+        await courseContentSubSection.save();
+
+        return res.status(200).json({
+            succcess: true,
+            message: `Sub Section "${title}" updated successfully`,
+            subSectionDetails: subSectionDetails
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error while updating sub-section",
+            error: error.message
+        })
+    }
+};
+
+// Delete the Course Content SubSection
+exports.deleteSubSection = async (req, res) => {
+
+    try {
+        // Fecth data from Req body
+        const { subSectionId, sectionId } = req.body;
+
+        // Remove the Sub Section ObjectId from Course Content Section
+        const courseContentSection = await CourseContentSection.findById(sectionId);
+        courseContentSection.subSection.pull(subSectionId);
+        await courseContentSection.save();
+
+        // Delete the sub-section
+        const subSectionDetails = await CourseContentSubSection.findByIdAndDelete(subSectionId);
+
+        // Validate the data
+        if (!subSectionDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Sub Section not found"
+            })
+        }
+
+        return res.status(200).json({
+            succcess: true,
+            message: `Sub Section "${subSectionDetails.title}" deleted successfully`,
+            subSectionDetails: subSectionDetails
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error while deleting sub-section",
+            error: error.message
+        })
+    }
+};
