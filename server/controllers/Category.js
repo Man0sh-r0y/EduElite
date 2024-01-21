c3bc23b0d80671bdeb291f08447b7031f1c2b9bf
Manash Roy <manashroy454@gmail.com>
2024-01-22 00:38:50 +0530

    updated the file

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -48,7 +48,7 @@ exports.getAllCategories = async (req, res) => {
     res.status(200).json({
         success: true,
         message: 'All Categories fetched successfully',
-        categories
+        categories: categories
     });
   } catch (error) {
     return res.status(500).json({
