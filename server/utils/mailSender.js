25ff1c6c484ebccc93e0b6798639b7e5bebe418c
Manash Roy <manashroy454@gmail.com>
2024-01-22 15:28:02 +0530

    updated the file

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -5,10 +5,10 @@ const mailSender = async (email, title, body) => {
     try{
         // send mail with defined transport object
         return await transporter.sendMail({
-            from: `StudyNotion`,
+            from: `${process.env.MAIL_USER}`,
             to: `${email}`,
             subject: `${title}`,
-            text: `${body}`
+            html: `${body}`
         });
     } catch (error) {
         console.log(`Error occured while sending mail through transporter: ${error}`);
